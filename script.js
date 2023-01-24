d79c77b2c876ce66438abd36e2feb8d4e2ecb213
Ravi Kumar <ravi.rks4488@gmail.com>
2023-01-24 23:07:29 +0530

    Removed Light-Dark Mode

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 (function () {
   var browserZoomLevel =
   Math.round(window.devicePixelRatio*100);
-  document.querySelector(".theme-btn").addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-  });
 })();
